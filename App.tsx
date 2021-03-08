56551f6baca9cc0fc899c6a6b9e408bbfbbbf5c1
Darshak Parmar <>
2021-03-08 19:44:08 +0530

    Splash iOS

--- App.tsx
+++ App.tsx
@@ -5,13 +5,18 @@
  * @format
  * @flow strict-local
  */
-
 import React, {Component} from 'react';
 import {View, StyleSheet} from 'react-native';
+// Splash Screen
+import SplashScreen from 'react-native-splash-screen';
 // Mobile Pages
 import QuotationPage from './src/pages/QuotationPage';
 
 class App extends Component {
+
+  componentDidMount() {
+    SplashScreen.hide();
+  }
   render() {
     return (
       <View style={styles.screenContainer}>
