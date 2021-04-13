69fa86069cb2d115c011d673b55becfea75d4abd
Darshak Parmar <>
2021-04-13 14:35:48 +0530

    Back to Normal without Backend Integrated

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -96,75 +96,75 @@ const LoginScreen = (props: any) => {
     console.log('Sign IN');
   }
 
-  function googleSignInCognitoHandler() {
-    GoogleSignin.hasPlayServices()
-      .then(() => {
-        GoogleSignin.signIn().then((userInfo) => {
-          console.log('userInfo', userInfo);
-          Auth.federatedSignIn(
-            'google',
-            {
-              token: userInfo.idToken,
-              expires_at: 60 * 1000 + new Date().getTime(), // the expiration timestamp
-            },
-            userInfo.user,
-          )
-            .then((cred) => {
-              // If success, you will get the AWS credentials
-              console.log('credentials ---->', cred);
-              return Auth.currentAuthenticatedUser();
-            })
-            .then((user) => {
-              // If success, the user object you passed in Auth.federatedSignIn
-              console.log('user after federated login -->', user);
-            })
-            .catch((e) => {
-              console.log('federated login error', e);
-            });
-          idToken = userInfo.idToken; // Get the Users ID token
-          console.log(userInfo);
-          setError(null);
-          setIsLoggedIn(true);
-          storeLoginToken(idToken);
-          storeUserInfo(userInfo);
-          props.navigation.replace('CameraModule');
-        });
-      })
-      .catch((error) => {
-        console.log('userInfo error', error);
-        if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-          {
-            /*-------When User cancel Sign In Progress-----*/
-            /*-------User Cancelled the login flow -----*/
-          }
-          Alert.alert('PROCESS HAS BEEN CANCELLED', error.code);
-          console.log('SIGN_IN_CANCELLED', error.code);
-        } else if (error.code === statusCodes.IN_PROGRESS) {
-          {
-            /*-------When in Progress Ready-------*/
-            /*----- operation (e.g. sign in) is in progress already -----*/
-          }
-          Alert.alert('PROCESS HAS IN PROGRESS', error.code);
-          console.log('IN_PROGRESS', error.code);
-        } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-          {
-            /*--------When Play Services not Available------*/
-            /*--------play services not available or outdated ------*/
-          }
-          Alert.alert('PLAY SERVICES ARE NOT AVAILABLE', error.code);
-          console.log('PLAY_SERVICES_NOT_AVAILABLE', error.code);
-        } else {
-          {
-            /*--------Some Other Errors-------*/
-            /*-------- Some other error happened ------*/
-          }
-          console.log(error);
-          Alert.alert('SOMETHING ELSE WENT WRONG');
-          console.log('other error', error.code);
-          setError(error);
-        }
-      });
-  }
+  // function googleSignInCognitoHandler() {
+  //   GoogleSignin.hasPlayServices()
+  //     .then(() => {
+  //       GoogleSignin.signIn().then((userInfo) => {
+  //         console.log('userInfo', userInfo);
+  //         Auth.federatedSignIn(
+  //           'google',
+  //           {
+  //             token: userInfo.idToken,
+  //             expires_at: 60 * 1000 + new Date().getTime(), // the expiration timestamp
+  //           },
+  //           userInfo.user,
+  //         )
+  //           .then((cred) => {
+  //             // If success, you will get the AWS credentials
+  //             console.log('credentials ---->', cred);
+  //             return Auth.currentAuthenticatedUser();
+  //           })
+  //           .then((user) => {
+  //             // If success, the user object you passed in Auth.federatedSignIn
+  //             console.log('user after federated login -->', user);
+  //           })
+  //           .catch((e) => {
+  //             console.log('federated login error', e);
+  //           });
+  //         idToken = userInfo.idToken; // Get the Users ID token
+  //         console.log(userInfo);
+  //         setError(null);
+  //         setIsLoggedIn(true);
+  //         storeLoginToken(idToken);
+  //         storeUserInfo(userInfo);
+  //         props.navigation.replace('CameraModule');
+  //       });
+  //     })
+  //     .catch((error) => {
+  //       console.log('userInfo error', error);
+  //       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+  //         {
+  //           /*-------When User cancel Sign In Progress-----*/
+  //           /*-------User Cancelled the login flow -----*/
+  //         }
+  //         Alert.alert('PROCESS HAS BEEN CANCELLED', error.code);
+  //         console.log('SIGN_IN_CANCELLED', error.code);
+  //       } else if (error.code === statusCodes.IN_PROGRESS) {
+  //         {
+  //           /*-------When in Progress Ready-------*/
+  //           /*----- operation (e.g. sign in) is in progress already -----*/
+  //         }
+  //         Alert.alert('PROCESS HAS IN PROGRESS', error.code);
+  //         console.log('IN_PROGRESS', error.code);
+  //       } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+  //         {
+  //           /*--------When Play Services not Available------*/
+  //           /*--------play services not available or outdated ------*/
+  //         }
+  //         Alert.alert('PLAY SERVICES ARE NOT AVAILABLE', error.code);
+  //         console.log('PLAY_SERVICES_NOT_AVAILABLE', error.code);
+  //       } else {
+  //         {
+  //           /*--------Some Other Errors-------*/
+  //           /*-------- Some other error happened ------*/
+  //         }
+  //         console.log(error);
+  //         Alert.alert('SOMETHING ELSE WENT WRONG');
+  //         console.log('other error', error.code);
+  //         setError(error);
+  //       }
+  //     });
+  // }
 
   async function signingOut() {
     try {
@@ -266,7 +266,7 @@ const LoginScreen = (props: any) => {
               color="#de4d41"
               backgroundColor="rgba(255,255,255,0.5)"
               onPress={() => {
-                googleSignInCognitoHandler();
+                signInHandler();
               }}
             />
           </TouchableWithoutFeedback>
