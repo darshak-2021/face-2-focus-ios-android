21bcde9c9ed88f7d1ba6ef83894db548b76d8f96
Darshak Parmar <>
2021-04-18 18:11:09 +0530

    Adding Activity Indicator to PickerSnap

--- src/pages/UserPickerPictureSnap.tsx
+++ src/pages/UserPickerPictureSnap.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,  {useState} from 'react';
 import {
   View,
   StyleSheet,
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   Platform,
   StatusBar,
+  ActivityIndicator
 } from 'react-native';
 import CloseOutline from 'react-native-vector-icons/Ionicons';
 import Button from '../components/Button';
@@ -14,8 +15,10 @@ import ImageResizer from 'react-native-image-resizer';
 import axios from 'axios';
 const PickerPicture = (props: any) => {
   console.log('Image is Picked Successfully: ', props.route.params.data)
+  const [fetching, setFetching] = useState(false)
 
   const generateBase64 = () => {
+    setFetching(true)
     ImageResizer.createResizedImage(props.route.params.data, 500, 800, 'JPEG', 50, 0, null)
       .then(response => {
         console.log('***********', response)
@@ -37,12 +40,16 @@ const PickerPicture = (props: any) => {
                 },
                 
               }).then(response => {
+                setFetching(false)
                 console.log('api response image detect', response.data?.FaceDetails[0]?.Emotions[0]?.Type)
                 const type = response.data?.FaceDetails[0].Emotions[0].Type;
                 props.navigation.navigate('MeditationListAudios', { emotion: type })
               }).catch(error => console.log('api image detect error', error));
             })
-            .catch(err => console.log('base64 error', err));
+            .catch(err => {
+              setFetching(false)
+              console.log('base64 error', err)
+            });
         // response.uri is the URI of the new image that can now be displayed, uploaded...
         // response.path is the path of the new image
         // response.name is the name of the new image with the extension
@@ -72,7 +79,9 @@ const PickerPicture = (props: any) => {
           <CloseOutline name="close-outline" size={40} color="white" />
         </TouchableOpacity>
         <View style={styles.buttonContainer}>
-          <Button
+        {fetching ? <View style = {{}}>
+              <ActivityIndicator size={'large'} color={'yellow'}/>
+            </View> : <Button
             title="Find Meditation"
             style={styles.buttonTextContainer}
             onClickButtonHandler={() =>{
@@ -80,7 +89,7 @@ const PickerPicture = (props: any) => {
               generateBase64();
             }
             }
-          />
+          /> }
         </View>
       </ImageBackground>
     </View>
